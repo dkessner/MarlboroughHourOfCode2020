def4ccd0cd73a87c4dd01b24d8040c7b83545769
Darren Kessner <Darren.Kessner@marlborough.org>
2020-11-28 07:48:36 -0800

    html structure cleanup

--- index.js
+++ index.js
@@ -4,13 +4,13 @@
 
 function initialize()
 {
-    // move p5.js default canvas to <div id="code">
+    // move p5.js default canvas to <div id="preview">
 
     let defaultCanvasElement = document.getElementById('defaultCanvas0');
-    let codeElement = document.getElementById('code');
+    let previewElement = document.getElementById('preview');
 
-    if (defaultCanvasElement && codeElement)
-        codeElement.appendChild(defaultCanvasElement);
+    if (defaultCanvasElement && previewElement)
+        previewElement.appendChild(defaultCanvasElement);
 }
 
 window.onload = initialize;
